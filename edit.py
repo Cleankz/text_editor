61d137182f27d8c30099e3605f08d08d36f6efa4
Cleankz <92687535+Cleankz@users.noreply.github.com>
2021-12-30 23:04:54 +0300

    Update edit.py

--- edit.py
+++ edit.py
@@ -1,32 +1,37 @@
 result = ""
 result_con = []
-metka = len(result_con) - 1
+metka = len(result_con)
 flag = False
-
+flag_1 = False
     
 def BastShoe(command):
-    global result,result_con,metka,flag
+    global result,result_con,metka,flag,flag_1
     string = command.split(" ",1)
     if int(string[0]) == 1:
-        metka = len(result_con) - 1
+        result = result + string[1]
+        result_con.append(result)
         if flag == True:
             result_con[len(result_con)-1] = result
             del result_con[0:len(result_con)-2]
             flag = False
-        result = result + string[1]
-        result_con.append(result)
+        if flag_1 == True:
+            flag_1 = False
+        else:
+            metka = len(result_con) - 1
+            flag_1 = False
         return result
     elif int(string[0]) == 2:
         array = list(result)
+        result = ''.join(array) 
+        result_con.append(result)
+        metka = len(result_con) - 1
         if int(string[1]) >= len(array):
             array.clear()
-            result = ''.join(array) 
+            result = ''
             result_con.append(result)
             if flag == True:
                 del result_con[0:len(result_con)-2]
                 flag = False
-            metka = len(result_con)
-            return result
         else:
             del array[len(array)-int(string[1]):len(array)]
             if flag == True:
@@ -35,39 +40,39 @@ def BastShoe(command):
                 flag = False
             result = ''.join(array)
             result_con.append(result)
-            metka = len(result_con) - 1
-            return result       
+            return result
+        return result
     elif int(string[0]) == 3:
         if int(string[1]) <= len(result)-1:
             flag = False
+            flag_1 = True
             return result[int(string[1])]
         else:
             flag = False
+            flag_1 = True
             return ""
     elif int(string[0]) == 4:
-        if len( result_con) <= 1 or len( result_con) <= 2 :
-            flag = True
+        flag = True
+        if len( result_con) <= 2 :
             result = result_con[0]
             return result_con[0]
         if metka <= 0:
-            flag = True
-            metka = 0
+            metka = len(result_con)-1
             result = result_con[0]
             return result_con[0]
-            
+        elif metka >= len(result_con):
+            metka = len(result_con) - 1
         metka = metka - 1
-        flag = True
         #result_con.append(result_con[metka])
         result = result_con[metka]
         return result_con[metka]
     elif int(string[0]) == 5:
-        if metka < len(result_con) -1:
-            flag = False
-            metka = metka + 1
-            return result_con[metka]
-        else: 
-            flag = False
+        flag = False
+        metka = metka + 1
+        if metka >= len(result_con):
             return result_con[len(result_con)-1]
+        else:
+            return result_con[metka]
     
     else:
          return result
